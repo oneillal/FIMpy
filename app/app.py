6a0ab748c8745d2be12e03b15e815174ccc47215
Alan O'Neill <alan.oneill@ie.ibm.com>
2017-10-30 11:39:38 +0000

    Added comparing digests

--- app/app.py
+++ app/app.py
@@ -35,7 +35,7 @@ CONFIG = {
     'password': 'password',
 # Array of paths to be monitored e.g. 'paths': ['/path1', '/path2/path3']
 #   'paths': ['/bin', '/usr/bin/python2.7']
-#   'paths': ['test']
+    'paths': ['test']
 }
 #######################################################################
 
@@ -46,9 +46,6 @@ app = Flask(__name__)
 
 # BUF_SIZE is totally arbitrary, change for your app!
 BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
-
-sha256 = hashlib.sha256()
-digest = hmac.new('secret-shared-key-goes-here', '', hashlib.sha256)
 
 client = None
 db = None
@@ -62,6 +59,7 @@ if 'VCAP_SERVICES' in os.environ:
         password = creds['password']
         url = 'https://' + creds['host']
         client = Cloudant(user, password, url=url, connect=True)
+        client.delete_database(CONFIG['db_name']) # delete existing db for dev
         db = client.create_database(CONFIG['db_name'], throw_on_exists=False)
 elif os.path.isfile('vcap-local.json'):
     with open('vcap-local.json') as f:
@@ -72,7 +70,15 @@ elif os.path.isfile('vcap-local.json'):
         password = creds['password']
         url = 'https://' + creds['host']
         client = Cloudant(user, password, url=url, connect=True)
+        client.delete_database(CONFIG['db_name']) # delete existing db for dev
         db = client.create_database(CONFIG['db_name'], throw_on_exists=False)
+
+        # Get all of the documents from my_database
+        for document in db:
+            # Look for all documents which do not match _design/
+            if not pattern.match(document['_id']):
+                print (document['_id'])
+                document.delete()
 
 @app.route('/')
 def home():
@@ -91,6 +97,10 @@ def home():
 @app.route('/api/fimpy', methods=['GET'])
 def getFileInfo():
     if client:
+        for document in db:
+            print(document['file'])
+            print('hash db> ' + document['hash'])
+            print('hmac db> ' + document['hmac'])
         return jsonify(list(map(lambda doc: doc['file'], db)))
     else:
         print('No database')
@@ -111,6 +121,8 @@ def writeFileData():
                 f = open(file, 'rb')
 #TODO add exception handling
                 try:
+                    sha256 = hashlib.sha256()
+                    digest = hmac.new('secret-shared-key-goes-here', '', hashlib.sha256)
                     while True:
                         block = f.read(BUF_SIZE)
                         if not block:
@@ -129,7 +141,7 @@ def writeFileData():
                             'ipaddress': socket.gethostbyname(socket.gethostname()),
                             'file': os.path.realpath(file),
                             'createdate': os.path.getmtime(file),
-                            'sha256': sha256.hexdigest(),
+                            'hash': sha256.hexdigest(),
                             'hmac': digest.hexdigest()}
                     db.create_document(data)
     return 'Records added to db'
@@ -148,6 +160,8 @@ def writeFileData():
 def scanFiles():
     if client:
         for document in db:
+            sha256 = hashlib.sha256()
+            digest = hmac.new('secret-shared-key-goes-here', '', hashlib.sha256)
             file = document['file']
             if os.path.isfile(file):
                 f = open(file, 'rb')
@@ -162,10 +176,15 @@ def scanFiles():
                 finally:
                     f.close()
             print(document['file'])
-            print(document['hmac'])
-            print(digest.hexdigest())
+            print('hash db> ' + document['hash'])
+            print('hash fs> ' + sha256.hexdigest())
+            print('hmac db> ' + document['hmac'])
+            print('hmac fs> ' + digest.hexdigest())
+
             if digest.hexdigest() == document['hmac']:
                 print "match"
+            else:
+                print "data does not match"
 
         return jsonify(list(map(lambda doc: doc['file'], db)))
     else:
